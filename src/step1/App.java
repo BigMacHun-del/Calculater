369090f33bae0c551d1c0a9fc172caae200f7166
김대훈 <eogns136@gmail.com>
2025-12-04 11:39:30 +0900

    step_1: 양의 정수, 사칙연산 기호  입력 받기  완료

--- src/step1/App.java
+++ src/step1/App.java
@@ -1,4 +1,18 @@
 package step1;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int value1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int value2 = sc.nextInt();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operations = sc.next().charAt(0);
+
+    }
 }
